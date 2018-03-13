9787538e2e353b954d22fba610dc0f91c71c8538
SapFir <sapfiryt@gmail.com>
2018-03-13 19:35:10 +0200

    Added User Interface.

---
+++ Hero_Fights/Menu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HeroFight
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : UserControl
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            btnFightStory.Completed += (s, ev) => 
+            { (this.Parent as MainWindow).SetMainElement(new PlayerMenu());
+            };
+            btnExitStory.Completed += (s, ev) => { Environment.Exit(0); };
+        }
+
+       
+    }
+}
