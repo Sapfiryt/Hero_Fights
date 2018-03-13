365d6845fe4f9da20fb4416e774d4903df57a29d
SapFir <sapfiryt@gmail.com>
2018-03-13 19:32:01 +0200

    Added Observers Pattern.

---
+++ Hero_Fights/DamageObserver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroFight
+{
+    public class DamageObserver : IObserver
+    {
+        int currentHP;
+        public void ObjectCreated(IHero hero)
+        {
+            currentHP = hero.HP;
+        }
+
+        public void ObjectModified(IHero hero)
+        {
+            var damage = hero.HP - currentHP;
+            currentHP = hero.HP;
+            if(damage<0)
+                BattleField.strLog += String.Format("\nHero {0} damaged on: {1}", hero.Name, damage);
+            //Console.WriteLine(String.Format("Hero {0} damaged on: {1}",hero.Name,damage));
+        }
+    }
+}
